f56908525eda1be9c591cb2f62b3e6e170b1452b
VlxdOS <v1adosek@yandex.ru>
2026-03-19 08:24:40 +0700

    add delete_server

--- database/requests.py
+++ database/requests.py
@@ -1,7 +1,7 @@
 # database/requests.py
 from database.core import async_session_maker
 from database.models import Server
-from sqlalchemy import select
+from sqlalchemy import select, delete
 
 async def get_servers():
     async with async_session_maker() as session:
@@ -11,3 +11,11 @@ async def get_servers():
         result = await session.execute(stmt)
         # scalars().all() превращает результат в чистый список объектов
         return result.scalars().all()
+    
+async def delete_server(ip: str):
+    """Удаляет сервер по IP"""
+    async with async_session_maker() as session:
+        # Удаляем строки, где ip равен переданному
+        stmt = delete(Server).where(Server.ip == ip)
+        await session.execute(stmt)
+        await session.commit()
